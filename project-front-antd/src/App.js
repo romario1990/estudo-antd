c38dcdfcd2d772fd9ac7b739fdb7c939abedf73f
romario1990 <romario.getulio@gmail.com>
2020-03-09 10:40:41 -0300

    creaded menu principal

--- project-front-antd/src/App.js
+++ project-front-antd/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Sider from "./menu";
 
 function App() {
-  return <div className="App">Olá mundano</div>;
+  return (
+    <div className="App">
+      <Sider></Sider>
+    </div>
+  );
 }
 
 export default App;
